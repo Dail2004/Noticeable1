84f13ed564b00fed74ea67986c4fcee2059406c0
Dail2004 <plusulta20@gmail.com>
2021-08-25 09:59:37 +1400

    Добавил добавление по нажатию фото

--- app/src/main/java/com/example/noticeable/MainActivity.java
+++ app/src/main/java/com/example/noticeable/MainActivity.java
@@ -1,11 +1,21 @@
 package com.example.noticeable;
 
+import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Base64;
+import android.view.View;
+import android.widget.ImageView;
 
 import com.example.noticeable.constant.PrefHelper;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
 
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -15,10 +25,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.noticeable.databinding.ActivityMainBinding;
 
+import java.io.ByteArrayOutputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+
 public class MainActivity extends AppCompatActivity {
 
     private AppBarConfiguration mAppBarConfiguration;
     private ActivityMainBinding binding;
+    private ImageView imageView;
+    private SharedPreferences sharedPreferences;
 
 
     @Override
@@ -40,14 +56,49 @@ public class MainActivity extends AppCompatActivity {
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
 
-
-
         binding.appBarMain.fab.setOnClickListener(view -> {
             navController.navigate(R.id.action_nav_home_to_noteFragment);
         });
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
         navigatorChangerListener(navController);
+        imageclickToGallery();
+        saveImageFromGallery();
+    }
+
+
+    ActivityResultLauncher<String> mGetContent = registerForActivityResult(new ActivityResultContracts.GetContent(),
+                uri -> {
+                    try {
+                        final InputStream imageStream = getContentResolver().openInputStream(uri);
+                        final Bitmap selectedImage = BitmapFactory.decodeStream(imageStream);
+                        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                        selectedImage.compress(Bitmap.CompressFormat.JPEG, 100, baos); //bm is the bitmap object
+                        byte[] b = baos.toByteArray();
+                        String encodedImage = Base64.encodeToString(b, Base64.DEFAULT);
+                        sharedPreferences.edit().putString("image", encodedImage).commit();
+                        imageView.setImageURI(uri);
+                    } catch (FileNotFoundException e) {
+                        e.printStackTrace();
+                    }
+                });
+    private void saveImageFromGallery() {
+        sharedPreferences=getSharedPreferences("", MODE_PRIVATE);
+        if (!sharedPreferences.getString("image","").equals("")){
+            byte[] decodedString = Base64.decode(sharedPreferences.getString("image", ""), Base64.DEFAULT);
+            Bitmap decoctedByte = BitmapFactory.decodeByteArray(decodedString, 0, decodedString.length);
+            imageView.setImageBitmap(decoctedByte);
+        }
+    }
+
+    private void imageclickToGallery() {
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        View view = navigationView.getHeaderView(0);
+        imageView = view.findViewById(R.id.imageView);
+        imageView.setOnClickListener(view1 -> {
+            mGetContent.launch("image/*");
+        });
+
     }
 
     private void navigatorChangerListener(NavController navController) {
